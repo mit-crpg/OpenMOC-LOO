4d98d026f652b4b1ce2ef05902655d70aa308d3f
lilulu6@gmail.com <lilulu6@gmail.com@e265fa28-6dd1-6ed6-75fe-3e1483af1a74>
2012-01-27 02:30:03 +0000

    Change main() to use stack allocation
    Using new was unnecessary here, and if you call new then you need to
    also call delete (which people will forget) in order to have a clean
    valgrind run.  Changing to stack allocation prevents needing to call
    delete for a lot of things, and if people forget to call delete then
    valgrind won't be clean, which is a pain.

--- src/openmoc.cpp
+++ src/openmoc.cpp
@@ -22,58 +22,57 @@
 
 // TODO: This is just stubbed out for now
 int main(int argc, const char **argv) {
-
 	log_printf(NORMAL, "Starting OpenMOC...\n");
 
 	/* Create an options class to parse command line options */
-	Options opts = Options(argc, argv);
+	Options opts(argc, argv);
 
 	/* Set the verbosity */
 	log_setlevel(opts.getVerbosity());
 
 	/* Create an empty geometry */
-	Geometry* geometry = new Geometry();
+	Geometry geometry;
 
+	/* FIXME: These should be part of the constructor */
 	/* Set the geomery's number of rings, sectors, and sector offset */
-	geometry->setNumRings(opts.getNumRings());
-	geometry->setNumSectors(opts.getNumSectors());
-	geometry->setSectorOffset(opts.getSectorOffset());
-
+	geometry.setNumRings(opts.getNumRings());
+	geometry.setNumSectors(opts.getNumSectors());
+	geometry.setSectorOffset(opts.getSectorOffset());
 
 	// FIXME: PUT THIS INSIDE THE PARSER TO HIDE IT FROM THE MAIN PROGRAM
 	/* We only want general functional calls inside the main program
-	 * The logic going on here is too specific and should be hidden from main
-	 */
+	 * The logic going on here is too specific and should be hidden from
+	 * main */
 	/* Initialize the parser */
-	Parser* parser = new Parser(&opts);
-	parser->each_surface([geometry](Surface *s) -> void
-			     {
-				     geometry->addSurface(s);
-				     return;
-			     });
-	parser->each_cell([geometry](Cell *c) -> void
-			  {
-				  geometry->addCell(c);
-				  return;
-			  });
+	Parser parser(&opts);
+	parser.each_surface([&geometry](Surface *s) -> void
+			    {
+				    geometry.addSurface(s);
+				    return;
+			    });
+	parser.each_cell([&geometry](Cell *c) -> void
+			 {
+				 geometry.addCell(c);
+				 return;
+			 });
 	
 
-	parser->each_lattice([geometry](Lattice *l) -> void
+	parser.each_lattice([&geometry](Lattice *l) -> void
 			  {
-				  geometry->addLattice(l);
+				  geometry.addLattice(l);
 				  return;
 			  });
 	
 	/* Adjust the indices for each geometry class to use uids */
-	geometry->adjustKeys();
+	geometry.adjustKeys();
 
 	/* Generate the neighbor cells for each surface in geometry */
-	geometry->buildNeighborsLists();
+	geometry.buildNeighborsLists();
 
 	/* Print out geometry to console */
-	log_printf(INFO, geometry->toString());
+	log_printf(INFO, geometry.toString());
 
 	/* Initialize the trackgenerator */
-	TrackGenerator* trackGenerator = new TrackGenerator(geometry,
+	TrackGenerator* trackGenerator = new TrackGenerator(&geometry,
 			opts.getTrackSpacing(), opts.getTrackSpacing());
 }
