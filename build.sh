62e2efe0454c6c94f73dbddb1f43f07ae7dd0d0c
Lulu Li <lilulu6@gmail.com>
2012-04-08 16:23:02 -0400

    Basic build script

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -ex
+
+CFLAGS="-Wall -Werror -std=gnu++0x `pkg-config Magick++ --cflags` -fopenmp -O3 -march=native"
+LDFLAGS="-lexpat -lsilo `pkg-config Magick++ --libs`"
+
+if [ "$1" = '--release' ]
+then
+    CFLAGS="$CFLAGS"
+elif [ "$1" = "--debug" ]
+then
+    CFLAGS="-Wall -Werror -std=gnu++0x `pkg-config Magick++ --cflags` -fopenmp -g"
+elif [ "$1" = '--profile' ]
+then
+    CFLAGS="$CFLAGS -pg -g"
+elif [ "$1" = "--gprofile" ]
+then
+    CFLAGS="$CFLAGS -lprofiler -g"
+fi
+
+if ! test -d bin
+then
+	mkdir bin
+fi
+gcc src/*.cpp -o bin/openmoc $CFLAGS $LDFLAGS
+
+if [ "$1" == "--profile" ]
+then
+    rm gmon.out || true
+    ./spectrum > /dev/null
+    gprof ./spectrum
+    rm gmon.out || true
+elif [ "$1" == "--gprofile" ]
+then
+    export CPUPROFILE_FREQUENCY=1000
+    export CPUPROFILE=gprofile.prof
+    ./spectrum > /dev/null
+    pprof --gv ./spectrum gprofile.prof
+fi
