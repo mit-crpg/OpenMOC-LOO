mit-crpg/OpenMOC-LOO

commit d2a0d48769de94643d6de991add8287a55d018d1

Add _converged flag: only save boundary flux after convergence
When _ub is on, There is no reason to save quad_flux at every
LOO iteration; but rather now there is a _converged flag that got
turned on after converged, and one more iteration of LOO is ran
where the angular quadrature fluxes are saved.

Current issue is still that simple cases (like 2x2 etc) do not
converge with -wl1 -ro, -wl2 -ro

src/Cmfd.cpp
@@ -127,6 +127,8 @@ Cmfd::Cmfd(Geometry* geom, Plotter* plotter, Mesh* mesh,
         _any_reflective = true;
     else
         _any_reflective = false;
+
+    _converged = false;
 }
 
 /**
@@ -1498,6 +1500,7 @@ double Cmfd::computeLooFluxPower(int moc_iter, double k_MOC)
     }
 
     _keff = k_MOC;
+    _converged = false;
     MeshCell* meshCell;
 
     /* Allocate memories for terms internal to the LOO iterative solver */
@@ -1648,7 +1651,7 @@ double Cmfd::computeLooFluxPower(int moc_iter, double k_MOC)
                         flux = 0.0; 
                     }
 	
-                    if (_update_boundary && _any_reflective)
+                    if (_converged && _update_boundary)
                     {
                         int surf_id = onReflectiveBoundary(t, i, 0);
 
@@ -1740,7 +1743,7 @@ double Cmfd::computeLooFluxPower(int moc_iter, double k_MOC)
                         flux = 0.0;
                     }
 
-                    if (_update_boundary && _any_reflective)
+                    if (_converged && _update_boundary)
                     {
                         int surf_id = onReflectiveBoundary(t, i, 1);
 
@@ -1903,6 +1906,17 @@ double Cmfd::computeLooFluxPower(int moc_iter, double k_MOC)
         log_printf(DEBUG, "%d: %.10f / (%.10f + %.10f) = %f", 
                    loo_iter, fis_tot, abs_tot, leak_tot, _keff);
 
+        if (_converged)
+        {
+            if (_plotter->plotKeff())
+                _plotter->plotCMFDKeff(_mesh, moc_iter);
+
+            if (_plotter->plotCurrent())
+                _plotter->plotNetCurrents(_mesh, moc_iter);
+
+            break;
+        }
+
         /* Computes the L2 norm of point-wise-division of energy-integrated
          * fission source of mesh cells between LOO iterations */
         eps = 0;
@@ -1942,18 +1956,10 @@ double Cmfd::computeLooFluxPower(int moc_iter, double k_MOC)
 
         /* If LOO iterative solver converges */
         if ((eps < _l2_norm_conv_thresh) && (loo_iter > min_outer))
-        {
-            if (_plotter->plotKeff())
-                _plotter->plotCMFDKeff(_mesh, moc_iter);
-
-            if (_plotter->plotCurrent())
-                _plotter->plotNetCurrents(_mesh, moc_iter);
-
-            break;
-        }
+            _converged = true;
     } 
 
-    _num_iter_to_conv = loo_iter + 1;
+    _num_iter_to_conv = loo_iter;
 
     /* Computes the L2 norm of point-wise-division of energy-integrated
      * fission source of mesh cells relative to (m+1/2) */

src/Cmfd.h
@@ -89,6 +89,7 @@ private:
     bool _any_reflective;
     bool _update_boundary;
     bool _reflect_outgoing;
+    bool _converged;
 public:
     Cmfd(Geometry* geom, Plotter* plotter, Mesh* mesh, 
          TrackGenerator *track_generator, Options *opts);

commit 7c46e1924bdf5a4bf80bad0144a61a62a2a84838

Fixed bug in commit d2a0d48769de94643d6de991add8287a55d018d1
commit d2a0d48769de94643d6de991add8287a55d018d1 introduced a
converged flag that is on when LOO converged, and run on more
loo iteration to store the boundary fluxes. Except the first
couple of transport sweeps where the max # of LOO sweeps are
reached, in these case we need to force LOO to store at the
last iteration (max_outer - 1).

src/Cmfd.cpp
@@ -1046,7 +1046,7 @@ void Cmfd::computeQuadSrc()
                             in[e][6] = s[0]->getQuadFlux(e,3);
                         }
 
-                        // FIXME? Debug?
+                        // FIXME? 
                         /*
                         s[0]->setQuadFlux(in[e][5], e, 1);
                         s[0]->setQuadFlux(in[e][6], e, 0);
@@ -1973,6 +1973,9 @@ double Cmfd::computeLooFluxPower(int moc_iter, double k_MOC)
 
         /* If LOO iterative solver converges */
         if ((eps < _l2_norm_conv_thresh) && (loo_iter > min_outer))
+            _converged = true;
+
+        if (loo_iter > max_outer - 3)
             _converged = true;
     } 
 